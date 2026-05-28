00dd8619ee619b25452dd533f6d870f35bd89aa0
Buzzcoder00 <linsonlivin@gmail.com>
2026-05-28 12:53:54 +0530

    gemini_integration

--- rag.py
+++ rag.py
@@ -1,5 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from google import genai
+
+client = genai.Client()
+
+
 while True:
-    query=input("Enter your query")
+    query=input("Enter your query:")
+
     if query.lower() in ("exit","quit"):
         break
-    print(f"you entered {query}")
+    
+    response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents=query
+    )
+    print(response.text)
